9155e55c5985337dc7dbd197b6dcd1b40c015657
leonid <lenagovorec58@gmail.com>
2024-04-07 11:49:41 +0300

    fix:optimize code

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -4,37 +4,38 @@ import { ActiveLink, Link, PaginationDiv } from 'components/Pagination/styled';
 import { IPagination } from 'utils/IPagination';
 
 export function Pagination({ data, setPage }: IPagination) {
+	const currentPage = data.pagination.current_page;
 	return (
 		<PaginationDiv>
 			{data.pagination.prev_url ? (
 				<>
-					<Link onClick={() => setPage(data.pagination.current_page - 1)}>
+					<Link onClick={() => setPage(currentPage - 1)}>
 						<img src={backArrow} alt="" />
 					</Link>
-					{data.pagination.current_page - 2 > 0 ? (
-						<Link onClick={() => setPage(data.pagination.current_page - 2)}>
-							{data.pagination.current_page - 2}
+					{currentPage - 2 > 0 ? (
+						<Link onClick={() => setPage(currentPage - 2)}>
+							{currentPage - 2}
 						</Link>
 					) : null}
-					<Link onClick={() => setPage(data.pagination.current_page - 1)}>
-						{data.pagination.current_page - 1}
+					<Link onClick={() => setPage(currentPage - 1)}>
+						{currentPage - 1}
 					</Link>
 				</>
 			) : null}
-			<ActiveLink onClick={() => setPage(data.pagination.current_page)}>
-				{data.pagination.current_page}
+			<ActiveLink onClick={() => setPage(currentPage)}>
+				{currentPage}
 			</ActiveLink>
 			{data.pagination.next_url ? (
 				<>
-					<Link onClick={() => setPage(data.pagination.current_page + 1)}>
-						{data.pagination.current_page + 1}
+					<Link onClick={() => setPage(currentPage + 1)}>
+						{currentPage + 1}
 					</Link>
-					{data.pagination.current_page + 2 <= data.pagination.total_pages ? (
-						<Link onClick={() => setPage(data.pagination.current_page + 2)}>
-							{data.pagination.current_page + 2}
+					{currentPage + 2 <= data.pagination.total_pages ? (
+						<Link onClick={() => setPage(currentPage + 2)}>
+							{currentPage + 2}
 						</Link>
 					) : null}
-					<Link onClick={() => setPage(data.pagination.current_page + 1)}>
+					<Link onClick={() => setPage(currentPage + 1)}>
 						<img src={arrow} alt="" />
 					</Link>
 				</>
