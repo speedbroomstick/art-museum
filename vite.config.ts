f808fdc6058602c97614f02b9e7a0ccf3e1ed8fc
leonid <lenagovorec58@gmail.com>
2024-04-10 22:09:52 +0300

    fix: delete useless files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-	base: '/art-museum/',
 	plugins: [react()],
 	test: {
 		globals: true,
